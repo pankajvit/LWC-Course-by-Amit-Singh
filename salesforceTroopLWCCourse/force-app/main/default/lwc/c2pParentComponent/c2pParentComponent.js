c2a3c6dcc7aee3892086758ddd108cffc3ddafb9
Pankaj Kumar <pankajvitmca@gmail.com>
2023-06-06 21:06:08 +0530

    child to parent communication through simple event

---
+++ salesforceTroopLWCCourse/force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class C2pParentComponent extends LightningElement {
+    showModal = false;
+
+    clickHandler(){
+        this.showModal = true;
+    }
+
+    closeHandler(){
+        this.showModal = false;
+    }
+}
