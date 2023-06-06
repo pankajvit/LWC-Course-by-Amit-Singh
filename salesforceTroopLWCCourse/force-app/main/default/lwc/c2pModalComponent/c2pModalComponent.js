21316e5ffbfa033d33e0482e636cb3af3f872cdc
Pankaj Kumar <pankajvitmca@gmail.com>
2023-06-07 01:22:12 +0530

    child to parent communication using bubble event

--- salesforceTroopLWCCourse/force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ salesforceTroopLWCCourse/force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -2,7 +2,15 @@ import { LightningElement } from 'lwc';
 
 export default class C2pModalComponent extends LightningElement {
     closeHandler(){
-        const myEvent = new CustomEvent('close');
+        const myEvent = new CustomEvent('close',{
+            bubbles: true,
+            detail: {
+                msg: "Modal Closed Successfully!!"
+            }
+        });
         this.dispatchEvent(myEvent);
+    }
+    footerHandler(){
+        console.log("footerHandler called");
     }
 }
