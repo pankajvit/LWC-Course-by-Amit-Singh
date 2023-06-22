4949d09c4d5fee5ab4c8358627a9effdaa7fbfe8
Pankaj Kumar <pankajvitmca@gmail.com>
2023-06-23 01:26:21 +0530

    pratice the code snippet for communication between parent to child through non-primitive data type

--- salesforceTroopLwcCourse2/force-app/main/default/lwc/p2cParentComponentPractice/p2cParentComponentPractice.js
+++ salesforceTroopLwcCourse2/force-app/main/default/lwc/p2cParentComponentPractice/p2cParentComponentPractice.js
@@ -1,3 +1,27 @@
 import { LightningElement } from 'lwc';
 
-export default class P2cParentComponentPractice extends LightningElement {}
+export default class P2cParentComponentPractice extends LightningElement {
+    crouselDetails = [
+        {
+            Id: 1,
+            src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",
+            header : "First Card",
+            description : "First card description.",
+            alternativetext : "First card accessible description."    
+        },
+        {
+            Id: 2,
+            src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg",
+            header : "Second Card",
+            description : "Second card description.",
+            alternativetext : "Second card accessible description."
+        },
+        {
+            Id: 3,
+            src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg",
+            header : "Third Card",
+            description : "Third card description.",
+            alternativetext : "Third card accessible description."
+        }
+    ];
+}
